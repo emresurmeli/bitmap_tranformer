d48ce6d7f1be00ad7ffad52596967113fd5af551
Emre Surmeli <s.emre.s.8@gmail.com>
2015-05-03 16:21:44 -0700

    modularized, but not really

--- app.js
+++ app.js
@@ -16,17 +16,11 @@ fs.readFile('./img/bitmap1.bmp', function (err, data) {
 	bitMap.startAddress = bitMapBuff.readUInt32LE(10);
 	bitMap.width = bitMapBuff.readUInt32LE(18);
 	bitMap.height = bitMapBuff.readUInt32LE(22);
-	bitMap.colorPlanes = bitMapBuff.readUInt16LE(26);
-	bitMap.colorDepth =  bitMapBuff.readUInt16LE(28);
 	bitMap.imageSize = bitMapBuff.readUInt32LE(34);
-	bitMap.resH = bitMapBuff.readUInt32LE(38);
-	bitMap.resV = bitMapBuff.readUInt32LE(42);
-	bitMap.colorNum = bitMapBuff.readUInt32LE(46);
-	bitMap.colorImpt = bitMapBuff.readUInt32LE(50);
 	bitMap.colorPalate = bitMapBuff.readUInt32LE(54);
-	bitMap.readColorPalate = (function () {
+	bitMap.readColorPallete = (function () {
 	  var counter = 0;
-	  for (var i = 54; i < bitMap.startAddress ; i+=4) {
+	  for (var i = 54; i < bitMap.startAddress; i+=4) {
 	    pallete[counter] = {
 	    b: bitMapBuff.readUInt8(i),
 		g: bitMapBuff.readUInt8(i+1),
@@ -36,6 +30,14 @@ fs.readFile('./img/bitmap1.bmp', function (err, data) {
 		counter ++;
 	   }
 	})();
+	bitMap.writeColorPallate = (function () {
+		var counter = 0;
+		for(var i = 54; i < bitMap.startAddress; i+=4) {
+			pallete[counter] = [bitMapBuff.readUInt8(i).toString()];
+			counter ++;
+		}
+	})();
+	console.log(pallete.toString());
 	var transform = function (array) {
 	  // Here we use a Fischer-Yates shuffle to "trasnform" the bmp file.. 
 	  var m = array.length, t, i;
@@ -49,10 +51,12 @@ fs.readFile('./img/bitmap1.bmp', function (err, data) {
 	  }
 	  return array;
 	}
+	console.log(bitMapBuff);
 	console.log(bitMap);
 	transform(pallete);
-	pallete.write(255, 54, 256);
-	console.log(pallete);
+  	var randomPallete = pallete.toString();
+    bitMapBuff.write(randomPallete, 54, 255);
+    console.log(typeof(bitMapBuff));
   }
   fs.writeFile('./img/randomized.bmp', bitMapBuff, function(err) {
     if(err) {
